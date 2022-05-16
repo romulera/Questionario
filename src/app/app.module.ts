a136e5615ebc90a2a027f50e12c361d1839eb63b
Romulera <romulomilanese@gmail.com>
2022-05-16 10:20:52 -0300

    Segundo commit cadastro-questao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestaoComponent } from './questao/questao.component';
+import { CadastroQuestaoComponent } from './cadastro-questao/cadastro-questao.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestaoComponent
+    QuestaoComponent,
+    CadastroQuestaoComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
